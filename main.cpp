6ace6b4cfa898e894f40542b09008183f46f32cf
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-02 23:35:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
    int n , i=0 ;
     cin >> n;
-    if (nprimo(n,i) ==true) {
+    if (nprimo(n) ==true) {
         cout << "numero primo" << endl;
     }
     else {

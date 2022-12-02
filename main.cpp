272307172ad56489f7070ca6a52938eaec1ad2e0
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-02 16:49:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+
+using namespace std;
 
 int main(){
+  int a , b , i;
+bool risultato ;
+cin >> a ;
+risultato = nprimo(a , i) ;
+if (risultato ==true ){
+    cout << "numero primo" << endl;
+}
+else {
+    cout << "numero non primo" << endl;
+}
   return 0;
 }

c94a568643dc952c990a94260addfe26c13e996e
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-02 16:47:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+#include "libreriaUno.h"
 
+bool nprimo (int n , int i) {
+    int div=1 ;
+    bool risultato;
+    i=0;
+    while(i<=1 and div<=n/2) {
+        if (n%div==0) {
+         bool nprimo(i++) ;
+        }
+        div++;
+    }
+    if (i==1) {
+        risultato = true ;
+        return true;
+    }
+    else {
+        risultato = false ;
+       return false ;
+    }
+}
